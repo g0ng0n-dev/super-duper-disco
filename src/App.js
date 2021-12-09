a2f20fd825c2dbad2826842224bb2253e5f43acb
Gon Gis <g0ng0n.dev@gmail.com>
2021-12-09 19:38:10 +0100

    adding the styles for the page, the details component and also it styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Grid} from '@material-ui/core';
+import useStyles from './styles';
+import Details from './components/Details/Details';
 
 const App = (props) => {
+    const classes = useStyles();
     return (
         <div>
-            App
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details title="income"/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details title="expense"/>
+                </Grid>
+            </Grid>
         </div>
     )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid } from "@material-ui/core";
+
+const Main = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Main;
