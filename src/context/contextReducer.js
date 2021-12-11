502ce3f84003b29f2cc8ec39d4ef1d7b1dcd2920
Gon Gis <g0ng0n.dev@gmail.com>
2021-12-11 23:34:21 +0100

    adding the reducer to modify the state

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,8 +1,13 @@
 // Reducer => a function that takes in the old state, and an action => new state...
-
 const contextReducer = (state, action) => {
-
-
+    switch (action.type){
+        case'DELETE_TRANSACTION':
+            return state.filter((t) => t.id !== action.payload )
+        case 'ADD_TRANSACTION':
+            return [action.payload, ...state]
+        default:
+            return state;
+    }
 
 }
 
