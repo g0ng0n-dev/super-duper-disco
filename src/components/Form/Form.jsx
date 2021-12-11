f996f4129e063a95e0641f6b26921a61336944e8
Gon Gis <g0ng0n.dev@gmail.com>
2021-12-11 23:43:59 +0100

    adding the form on change values a

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import React, {useState} from 'react';
+import {Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography} from "@material-ui/core";
 import useStyles from './styles';
+
+const initialState = {
+    amount: '',
+    category: '',
+    type: 'Income',
+    date: new Date(),
+}
 
 const Form = props => {
     const classes = useStyles();
+    const [formData, setFormData] = useState(initialState);
+
 
     return (
         <Grid container spacing={2}>
@@ -15,7 +24,7 @@ const Form = props => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select>
+                    <Select value={formData.type} onChange={(e) => setFormData({...formData, type: e.target.value})}>
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -24,25 +33,27 @@ const Form = props => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select >
+                    <Select value={formData.category}
+                            onChange={(e) => setFormData({...formData, category: e.target.value})}>
                         <MenuItem value="business">Business</MenuItem>
                         <MenuItem value="salary">Salary</MenuItem>
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="number" label="Amount" fullWidth />
+                <TextField type="number" label="Amount" fullWidth value={formData.amount}
+                           onChange={(e) => setFormData({...formData, amount: e.target.value})}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth />
+                <TextField type="date" label="Date" fullWidth value={formData.date}
+                           onChange={(e) => setFormData({...formData, date: e.target.value})}/>
+                />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
         </Grid>
     );
 };
 
-Form.propTypes = {
-
-};
+Form.propTypes = {};
 
 export default Form;
